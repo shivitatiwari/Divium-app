8a64ed73ec2efb2798e6c43e4608e938e0c8415e
shivitatiwari <shivitatiwari@gmail.com>
2026-09-19 12:19:20 +0530

    build: migrate Kotlin JVM target DSL

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,8 +37,10 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlinOptions {
-        jvmTarget = "17"
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+        }
     }
 }
 
